13a2475a74cb18c5d85aef822202255a9c8d93cd
belen_amura@hotmail.com <belen_amura@hotmail.com>
2023-06-08 14:04:26 +0200

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Ejemplo de JavaScript para mostrar una alerta al hacer clic en el título
+const titulo = document.querySelector('h1');
+
+titulo.addEventListener('click', () => {
+  alert('¡Has hecho clic en el título!');
+});
